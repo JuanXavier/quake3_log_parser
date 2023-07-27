090823918f6c8d7630aab1ba14e6f2188876347b
Juan Xavier Valverde <juanxaviervm@hotmail.com>
2023-07-26 19:33:15 -0500

    `killsPerGame` commented(#9)

--- src/killsPerGame.ts
+++ src/killsPerGame.ts
@@ -1,9 +1,13 @@
+// Import the file system module to read and write files
 import * as fs from 'fs'
+// Import the readline module to read the log file line by line
 import * as readline from 'readline'
 
+// Initialize variables to store the current game number and game data
 let currentGame: any = 0
 let games: any = {}
 
+// This function extracts a player's name from a line in the log file
 const parsePlayerName = (line: any) => {
 	const regex = /n\\([^\\]+)/
 	const match = line.match(regex)
@@ -11,6 +15,7 @@ const parsePlayerName = (line: any) => {
 	if (match !== null) {
 		const playerName = match[1]
 
+		// If the player is not already in the current game's player list, add them
 		if (!games[`game_${currentGame}`].players.includes(playerName) && playerName != null) {
 			games[`game_${currentGame}`].players.push(playerName)
 		}
@@ -19,6 +24,7 @@ const parsePlayerName = (line: any) => {
 	return null
 }
 
+// This function processes a line from the log file that contains a kill event
 const parseKillLine = (line: string) => {
 	const killRegex = /(\d+:\d+) Kill: (\d+) (\d+) (\d+): (.+) killed (.+) by (.+)/
 	const match = line.match(killRegex)
@@ -27,20 +33,27 @@ const parseKillLine = (line: string) => {
 		const killer = match[5]
 		const victim = match[6]
 
+		// If the killer and victim are not already in the current game's player list, add them
 		if (killer !== '<world>' && !games[`game_${currentGame}`].players.includes(killer)) {
 			games[`game_${currentGame}`].players.push(killer)
 		}
+		if (!games[`game_${currentGame}`].players.includes(victim)) {
+			games[`game_${currentGame}`].players.push(victim)
+		}
 
-		if (!games[`game_${currentGame}`].players.includes(victim)) games[`game_${currentGame}`].players.push(victim)
-
+		// Increment the total kill count for the current game
 		games[`game_${currentGame}`].total_kills++
 
-		if (killer !== '<world>')
+		// Update the kill count for the killer or victim
+		if (killer !== '<world>') {
 			games[`game_${currentGame}`].kills[killer] = (games[`game_${currentGame}`].kills[killer] || 0) + 1
-		else games[`game_${currentGame}`].kills[victim] = (games[`game_${currentGame}`].kills[victim] || 0) - 1
+		} else {
+			games[`game_${currentGame}`].kills[victim] = (games[`game_${currentGame}`].kills[victim] || 0) - 1
+		}
 	}
 }
 
+// This function reads the log file line by line and processes each line
 const getKillData = async (filePath: string) => {
 	const fileStream = fs.createReadStream(filePath)
 	const rl = readline.createInterface({
@@ -48,11 +61,15 @@ const getKillData = async (filePath: string) => {
 		crlfDelay: Infinity,
 	})
 
+	// For each line in the log file...
 	for await (const line of rl) {
-		if (line.includes('ClientUserinfoChanged')) parsePlayerName(line)
-		else if (line.includes('Kill')) parseKillLine(line)
-		else if (line.includes('InitGame')) {
-			currentGame++ // New game starts
+		if (line.includes('ClientUserinfoChanged')) {
+			parsePlayerName(line)
+		} else if (line.includes('Kill')) {
+			parseKillLine(line)
+		} else if (line.includes('InitGame')) {
+			// If a new game starts, increment the current game number and initialize its data
+			currentGame++
 			games[`game_${currentGame}`] = {
 				total_kills: 0,
 				players: [],
@@ -70,32 +87,41 @@ const getKillData = async (filePath: string) => {
 		player_rankings: playerRankings,
 	}
 
-	// Write the output to the file
+	// Write the output to a JSON file
 	fs.writeFile('./output/Kills_per_game.json', JSON.stringify(output, null, 2), (err) => {
-		if (err) console.error('Error writing file: ', err)
-		else console.log('Successfully written to output/Kills_per_game.json')
+		if (err) {
+			console.error('Error writing file: ', err)
+		} else {
+			console.log('Successfully written to output/Kills_per_game.json')
+		}
 	})
 }
 
-// Function to compute player rankings based on kill counts
+// This function computes player rankings based on kill counts
 const computePlayerRankings = () => {
 	const playerRankings: any = {}
 
+	// For each game...
 	for (let gameNumber = 1; gameNumber <= currentGame; gameNumber++) {
 		const gameData = games[`game_${gameNumber}`]
 
+		// For each player in the game...
 		for (const playerName of gameData.players) {
 			const totalKills = gameData.kills[playerName] || 0
+
+			// Update the player's total kill count across all games
 			playerRankings[playerName] = (playerRankings[playerName] || 0) + totalKills
 		}
 	}
 
-	// Convert playerRankings object to an array of { playerName, kills } objects
+	// Convert the playerRankings object to an array of { playerName, kills } objects
 	const rankingsArray = Object.entries(playerRankings).map(([playerName, kills]) => ({ playerName, kills }))
 
 	// Sort the rankingsArray in descending order of kills
 	rankingsArray.sort((a: any, b: any) => b.kills - a.kills)
+
 	return rankingsArray
 }
 
+// Start the process by parsing the log file
 getKillData('./logs/Quake.log')
